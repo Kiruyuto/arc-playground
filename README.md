b2c608b168699b21c347567c09f4a06b8f3e94ce
Hubert@Home <h.zielinski.business@gmail.com>
2025-04-17 05:38:29 +0200

    feat: Add templates

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # arc-playground
+
+Playground for the self-hosted [Actions Runner Controller](https://github.com/actions/actions-runner-controller) (ARC)  
+
+> [!WARNING]
+> **Not intended for production use**.  
+> This stuff may be broken or break at any time.
+
+### Usefull links & Commands
+- [ARC repo](https://github.com/actions/actions-runner-controller)
+- [ARC docs](https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners-with-actions-runner-controller/about-actions-runner-controller)
+- [ARC charts dir](https://github.com/actions/actions-runner-controller/blob/master/charts)
+- Execute (Alias)
+  ```
+  kubeexec command_to_execute
+  ```
+- TBA
---
+++ deploy-all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+## Add openebs (Required when using containerMode.type=kubernetes)
+# helm repo add openebs https://openebs.github.io/openebs
+# helm repo update
+# helm install openebs --namespace openebs openebs/openebs --create-namespace
+
+# Deploy Controller
+helm install arc-controller \
+  --namespace arc-systems \
+  --create-namespace \
+  -f ~/arc-config/controller/values.yaml \
+  oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
+
+# Deploy Runner Scale Set
+helm install arc-runner \
+  --namespace arc-runners \
+  --create-namespace \
+  -f ~/arc-config/runner-scale-set/values.yaml \
+  oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
