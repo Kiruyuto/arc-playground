eb7b8f4199e81422f687ba243069bd0eeb2ae349
Hubert@Home <h.zielinski.business@gmail.com>
2025-04-28 22:36:50 +0200

    chore: Set script auto fail

--- custom-image/scripts/pre-job.sh
+++ custom-image/scripts/pre-job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+set -euo pipefail
 echo "Running docker image version [${DOCKER_IMAGE_VERSION}]."
 
-if [ -n "$GITHUB_TOKEN" ]; then
+if [ -n "${CLI_PAT_TOKEN:-}" ]; then
   echo "Logging into GitHub CLI with provided token."
-  echo "$GITHUB_TOKEN" | gh auth login --with-token
+  echo "$CLI_PAT_TOKEN" | gh auth login --with-token
 fi
 
 echo "Running pre-job script finished."
